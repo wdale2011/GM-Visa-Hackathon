f6fa375a9ec9a190a0db913b845025c36e7c0f59
David <dquiri253@gmail.com>
2018-11-25 15:09:35 -0800

    third time

---
+++ Car2Car/Car2Car/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Car2Car.WebApiApplication" Language="C#" %>
---
+++ reactgm/build/precache-manifest.cfa3c0042f024315d4d0e3a0dd4fcc72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6e0385b457860da0d798",
+    "url": "/static/js/main.6e0385b4.chunk.js"
+  },
+  {
+    "revision": "8da104c855a4a1f822b5",
+    "url": "/static/js/1.8da104c8.chunk.js"
+  },
+  {
+    "revision": "6e0385b457860da0d798",
+    "url": "/static/css/main.b85083ad.chunk.css"
+  },
+  {
+    "revision": "307bc0bec9d465c732c685691d5712f7",
+    "url": "/index.html"
+  }
+];
